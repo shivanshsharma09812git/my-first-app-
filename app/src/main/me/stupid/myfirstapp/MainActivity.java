c65efe44282ce08db923b2d52902e1b9c49d75a0
shivanshsharma09812git <sharmashivansh.09812@gmail.com>
2025-08-30 22:33:24 +0530

    Update and rename placeholder to MainActivity.java

---
+++ app/src/main/me/stupid/myfirstapp/MainActivity.java
@@ -0,0 +1,16 @@
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/root.html");
+    }
+}
